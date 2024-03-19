033518782cbc0645ed1379d517111c468f743891
KiwilDev <boskhilmiarked123@gmail.com>
2024-03-19 21:16:52 +0700

    auto display data in combobox

--- bukuhilmi/dashboard_penjualan.Designer.cs
+++ bukuhilmi/dashboard_penjualan.Designer.cs
@@ -31,20 +31,15 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.combo_buku = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_detailBuku = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button3
@@ -64,6 +59,7 @@
             this.button2.TabIndex = 32;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -74,86 +70,6 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(226, 276);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 30;
-            this.label6.Text = "label6";
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(284, 273);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 29;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(226, 250);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 28;
-            this.label5.Text = "label5";
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(284, 247);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 27;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(226, 221);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 26;
-            this.label4.Text = "label4";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(284, 218);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 25;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(25, 273);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 24;
-            this.label3.Text = "label3";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(83, 270);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 23;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 247);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 22;
-            this.label2.Text = "label2";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(83, 244);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 21;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -163,13 +79,6 @@
             this.label1.TabIndex = 20;
             this.label1.Text = "Buku";
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(83, 218);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 19;
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -178,30 +87,74 @@
             this.dataGridView1.Size = new System.Drawing.Size(764, 150);
             this.dataGridView1.TabIndex = 18;
             // 
+            // combo_buku
+            // 
+            this.combo_buku.FormattingEnabled = true;
+            this.combo_buku.Location = new System.Drawing.Point(83, 217);
+            this.combo_buku.Name = "combo_buku";
+            this.combo_buku.Size = new System.Drawing.Size(121, 21);
+            this.combo_buku.TabIndex = 34;
+            this.combo_buku.SelectedIndexChanged += new System.EventHandler(this.combo_buku_SelectedIndexChanged);
+            this.combo_buku.Click += new System.EventHandler(this.combo_buku_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label_detailBuku);
+            this.panel1.Location = new System.Drawing.Point(28, 249);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 122);
+            this.panel1.TabIndex = 35;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "detail buku";
+            // 
+            // label_detailBuku
+            // 
+            this.label_detailBuku.AutoEllipsis = true;
+            this.label_detailBuku.AutoSize = true;
+            this.label_detailBuku.Location = new System.Drawing.Point(12, 14);
+            this.label_detailBuku.Name = "label_detailBuku";
+            this.label_detailBuku.Size = new System.Drawing.Size(10, 13);
+            this.label_detailBuku.TabIndex = 0;
+            this.label_detailBuku.Text = "-";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(59, 216);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(25, 23);
+            this.button4.TabIndex = 37;
+            this.button4.Text = "🔃";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // dashboard_penjualan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.combo_buku);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.dataGridView1);
             this.Name = "dashboard_penjualan";
             this.Size = new System.Drawing.Size(808, 382);
+            this.Load += new System.EventHandler(this.dashboard_penjualan_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,18 +165,12 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox combo_buku;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_detailBuku;
+        private System.Windows.Forms.Button button4;
     }
 }
--- bukuhilmi/dashboard_penjualan.cs
+++ bukuhilmi/dashboard_penjualan.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,89 @@ namespace bukuhilmi
 {
     public partial class dashboard_penjualan : UserControl
     {
+        public SqlConnection connection = new SqlConnection(konek.conn);
+        public SqlCommand command;
+        public SqlDataAdapter adapter;
+        public DataSet dataSet;
+        public SqlDataReader reader;
         public dashboard_penjualan()
         {
             InitializeComponent();
+
+            showBukuInComboBox();
+            showDetailBuku_label();
+        }
+
+        public void showBukuInComboBox()
+        {
+            connection.Close();
+            connection.Open();
+
+            string sql = "SELECT * FROM buku";
+            command = new SqlCommand(sql, connection);
+            adapter = new SqlDataAdapter(command);
+            dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            combo_buku.DataSource = dataSet.Tables[0];
+            combo_buku.DisplayMember = "judul";
+            combo_buku.ValueMember = "id_buku";
+        }
+
+        public void showDetailBuku_label()
+        {
+            if (combo_buku.SelectedIndex != -1)
+            {
+                if (int.TryParse(combo_buku.SelectedValue.ToString(), out int selectedBookId))
+                {
+                    string sql1 = "SELECT * FROM buku WHERE id_buku = @id_buku";
+
+                    using (SqlCommand command = new SqlCommand(sql1, connection))
+                    {
+                        command.Parameters.AddWithValue("@id_buku", selectedBookId);
+                        using (SqlDataAdapter detail_adapter = new SqlDataAdapter(command))
+                        {
+                            DataSet detail_dataSet = new DataSet();
+                            detail_adapter.Fill(detail_dataSet);
+                            if (detail_dataSet.Tables[0].Rows.Count > 0)
+                            {
+                                DataRow row = detail_dataSet.Tables[0].Rows[0];
+                                string hargaFormatted = Convert.ToDecimal(row["harga_buku"]).ToString("C0", new CultureInfo("id-ID"));
+                                string detailBuku = $"Judul: {row["judul"]}\nPenulis: {row["penulis"]}\nPenerbit: {row["penerbit"]}\nTahun Terbit: {row["tahun"]}\nStok: {row["stok"]}\nHarga: {hargaFormatted}";
+                                label_detailBuku.Text = detailBuku;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void combo_buku_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dashboard_penjualan_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void combo_buku_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            showDetailBuku_label();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            showBukuInComboBox();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
